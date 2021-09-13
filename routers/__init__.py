fdf765f9043977cfb5475c600ae161e4d7914a54
PigeonsHouse <bullbull1230@gmail.com>
2021-09-13 21:25:26 +0900

    [Change] エンドポイントの仕様変更による修正

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,9 +3,11 @@ from .sign import sign_router
 from .scoring import scoring_router
 from .ranking import ranking_router
 from .users import user_router
+from .posts import post_router
 
 r = APIRouter()
 r.include_router(sign_router)
 r.include_router(scoring_router)
 r.include_router(ranking_router)
 r.include_router(user_router)
+r.include_router(post_router)
--- schemas/images.py
+++ schemas/images.py
@@ -2,12 +2,9 @@ from datetime import datetime
 from .posts import Post
 from pydantic import BaseModel, HttpUrl
 
-class BaseImage(BaseModel):
-    url: HttpUrl
-    post: Post
-
-class Post(BaseImage):
+class Image(BaseModel):
     id: str
+    url: HttpUrl
     created_at: datetime
     updated_at: datetime
 
